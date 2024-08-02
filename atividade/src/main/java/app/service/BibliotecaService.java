b8b5dd033d836326607a2f387f16629b2d66adb7
Isabela Migliorini <isabelamigli@gmail.com>
2024-08-01 23:14:30 -0300

    Primeiro commit. Entities e BibliotecaService

---
+++ atividade/src/main/java/app/service/BibliotecaService.java
@@ -0,0 +1,55 @@
+package app.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import app.entity.Biblioteca;
+
+@Service
+public class BibliotecaService {
+	
+	public String save(Biblioteca biblioteca) {
+		return "Biblioteca salva com sucesso!";
+	}
+	
+	public String update(Biblioteca biblioteca) {
+		return "Biblioteca atualizada com sucesso!";
+	}
+	
+	public List<Biblioteca> findAll() {
+		List<Biblioteca> lista = new ArrayList<>();
+		
+		lista.add(new Biblioteca(1, "Rua 1", "45 9 9999-9999"));
+		lista.add(new Biblioteca(2, "Rua 2", "45 9 9999-9999"));
+		lista.add(new Biblioteca(3, "Rua 3", "45 9 9999-9999"));
+		lista.add(new Biblioteca(4, "Rua 4", "45 9 9999-9999"));
+		lista.add(new Biblioteca(5, "Rua 5", "45 9 9999-9999"));
+		
+		return lista;
+	}
+	
+	public Biblioteca findById(long id) {
+		List<Biblioteca> lista = this.findAll();
+		
+		for(Biblioteca biblioteca : lista) {
+			if(biblioteca.getId() == id) {
+				return biblioteca;
+			}
+		}
+		return null;
+	}
+	
+	public String delete(long id) {
+		List<Biblioteca> lista = this.findAll();
+		
+		for(Biblioteca biblioteca : lista) {
+			if(biblioteca.getId() == id) {
+				return "Endereço da biblioteca deletada: " + biblioteca.getEndereco();
+			}
+		}
+		return "Biblioteca não encontrada";
+	}
+
+}
