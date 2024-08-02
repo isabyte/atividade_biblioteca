ede26af3307aab2e6fa8e6ae388c2d365da9a0bb
Isabela Migliorini <isabelamigli@gmail.com>
2024-08-02 00:23:19 -0300

    EditoraService

--- atividade/src/main/java/app/service/EditoraService.java
+++ atividade/src/main/java/app/service/EditoraService.java
@@ -1,5 +1,55 @@
 package app.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import app.entity.Editora;
+
+@Service
 public class EditoraService {
+	
+	public String save(Editora editora) {
+		return "Editora salva com sucesso!";
+	}
+	
+	public String update(Editora editora, long id) {
+		return "Editora atualizada com sucesso!";
+	}
+	
+	public List<Editora> findAll() {
+		List<Editora> lista = new ArrayList<>();
+		
+		lista.add(new Editora(1, "Editora 1", "Rua 1"));
+		lista.add(new Editora(2, "Editora 2", "Rua 2"));
+		lista.add(new Editora(3, "Editora 3", "Rua 3"));
+		lista.add(new Editora(4, "Editora 4", "Rua 4"));
+		lista.add(new Editora(5, "Editora 5", "Rua 5"));
+
+		return lista;
+	}
+	
+	public Editora findById(long id) {
+		List<Editora> lista = this.findAll();
+		
+		for (Editora editora : lista) {
+			if(editora.getId() == id) {
+				return editora;
+			}
+		}
+		return null;
+	}
+	
+	public String delete(long id) {
+		List<Editora> lista = this.findAll();
+		
+		for(Editora editora : lista) {
+			if(editora.getId() == id) {
+				return "Editora " + editora.getNome()+ " deletada com sucesso";
+			}
+		}
+		return "Erro ao deletar editora";
+	}
 
 }
