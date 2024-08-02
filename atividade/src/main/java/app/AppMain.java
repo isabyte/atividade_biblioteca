71d51e0a74396531ced6172e69c7e35a96b2d8f0
Isabela Migliorini <isabelamigli@gmail.com>
2024-08-02 00:00:21 -0300

    Main

--- atividade/src/main/java/app/AppMain.java
+++ atividade/src/main/java/app/AppMain.java
@@ -1,13 +1,13 @@
-package com.example.atividade;
+package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AtividadeApplication {
+public class AppMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AtividadeApplication.class, args);
+		SpringApplication.run(AppMain.class, args);
 	}
 
 }
