c9497b1d294f68158cdacb1dc25602ff652fa6f7
Isabela Migliorini <isabelamigli@gmail.com>
2024-08-08 15:45:30 -0300

    criação dos repositories

--- atividade/src/main/java/app/repository/EditoraRepository.java
+++ atividade/src/main/java/app/repository/EditoraRepository.java
@@ -1,5 +1,9 @@
 package app.repository;
 
-public class EditoraRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import app.entity.Editora;
+
+public interface EditoraRepository extends JpaRepository<Editora, Long> {
 
 }
