4f419734b761e5dae89d44b9dab3fb71e04f23ed
Isabela Migliorini <isabelamigli@gmail.com>
2024-08-08 15:39:53 -0300

    annotations entities

--- atividade/src/main/java/app/entity/Livro.java
+++ atividade/src/main/java/app/entity/Livro.java
@@ -1,7 +1,15 @@
 package app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Livro {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String titulo;
 	private int ano_publicacao;
